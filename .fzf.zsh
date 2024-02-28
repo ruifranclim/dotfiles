94498b21563a85c1d3eaeafd8dfb8cfc6701aa3d
rs <rui@fastmail.com>
2024-02-28 17:42:42 +0000

    fzf file copy

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,1 +1,15 @@
-/Users/ruifranclim/.fzf.zsh
+# Setup fzf
+# ---------
+if [[ ! "$PATH" == */opt/homebrew/opt/fzf/bin* ]]; then
+  PATH="${PATH:+${PATH}:}/opt/homebrew/opt/fzf/bin"
+fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/opt/homebrew/opt/fzf/shell/completion.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/opt/homebrew/opt/fzf/shell/key-bindings.zsh"
+source "${HOME}/.oh-my-zsh/custom/plugins/fzf-tab/fzf-tab.plugin.zsh"
+
